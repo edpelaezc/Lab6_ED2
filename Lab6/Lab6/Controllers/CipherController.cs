7e47c8ebe960f33cd8c7e709d3a1c97e51552d5c
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-04-24 01:01:24 -0600

    En criptografía, el cifrado César, también conocido como cifrado por desplazamiento, código de César o desplazamiento de César,

--- Lab6/Lab6/Controllers/CipherController.cs
+++ Lab6/Lab6/Controllers/CipherController.cs
@@ -2,23 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.IO;
+using lab6.Models;
 
 namespace lab6.Controllers
 {
     public class CipherController : Controller
-    {
+    {        
         public IActionResult Index()
         {
             return View();
         }
 
-        [HttpPost, ActionName("SendKeys")]
-        public void SendKeys(int a, int b)
-        {
-            Console.WriteLine(a+b);
+        [HttpPost, ActionName("getPublicKey")]
+        public IActionResult getPublicKey(int a, int b)
+        {           
             Diffie secretKey = new Diffie(a, b);
-            secretKey.generateKey();
+            Data.Instance.secretKey = secretKey.generateKey();
+
+            return RedirectToAction(nameof(Index));
         }
+
+        [HttpPost, ActionName("caesar2")]
+        public IActionResult caesar2(IFormFile file)
+        {
+            //lectura del archivo
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadLine());
+            }
+
+            string text = result.ToString();
+            text = text.Remove(text.Length - 1);
+            text = text.Remove(text.Length - 1);
+            Data.Instance.caesarCipher.cifrarCesar(text, Data.Instance.secretKey, file.FileName);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- lab6/lab6/Caesar.cs
+++ lab6/lab6/Caesar.cs
@@ -9,104 +9,57 @@ namespace lab6
 {
     public class Caesar
     {
-        string keyWord { get; set; }
-        List<char> newAlphabet = new List<char>();
-        List<char> originalAlphabet = new List<char>();
-        bool flag;
+        public Caesar() { }
 
-        public Caesar(string word)
+        private int searchInAlphabeth(String alphabet, char desiredChar)
         {
-            keyWord = word;
-        }
 
-        public void buildAlphabet()
-        {
-            List<byte> modified = new List<byte>();
-            List<byte> original = new List<byte>();
+            char[] chars = alphabet.ToCharArray();
 
-            byte[] keyWrdByt = Encoding.ASCII.GetBytes(keyWord);
-            modified.AddRange(keyWrdByt);
-
-            for (int i = 97; i < 123; i++)
+            for (int i = 0; i < chars.Length; i++)
             {
-                original.Add((byte)i);
-                modified.Add((byte)i);
-            }
-
-            modified = modified.Distinct().ToList();
-
-            for (int i = 0; i < original.Count; i++)
-            {
-                originalAlphabet.Add(Convert.ToChar(original[i]));
-            }
-
-            for (int i = 0; i < modified.Count; i++)
-            {
-                newAlphabet.Add(Convert.ToChar(modified[i]));
-            }
-
-
-        }
-
-        private int searchInAlphabet(char character)
-        {
-            for (int i = 0; i < originalAlphabet.Count; i++)
-            {
-                if (character == originalAlphabet[i])
-                {
-                    flag = false;
+                if (Char.ToUpper(desiredChar) == Char.ToUpper(chars[i]))
                     return i;
-                }
-                else if (character == Char.ToUpper(originalAlphabet[i]))
-                {
-                    flag = true; //si es true se escribe en mayuscula en el archivo encriptado
-                    return i;
-                }
             }
             return -1;
         }
 
-        private int searchInNewAlphabet(char character)
+        public void cifrarCesar(string frase, int distancia, string fileName)
         {
-            for (int i = 0; i < newAlphabet.Count; i++)
-            {
-                if (character == newAlphabet[i])
-                {
-                    flag = false;
-                    return i;
-                }
-                else if (character == Char.ToUpper(newAlphabet[i]))
-                {
-                    flag = true; //si es true se escribe en mayuscula en el archivo encriptado
-                    return i;
-                }
-            }
-            return -1;
-        }
+            String alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+            String nuevaFrase = "";
 
-        public void cipher(StringBuilder input, string fileName)
-        {
-            string encryptedText = "";
-            string text = input.ToString();
-            text = input.ToString();
-            //eliminar el caracter \r\n que agrega el stringbuilder al final de todo el texto
-            text = text.Remove(text.Length - 1);
-            text = text.Remove(text.Length - 1);
+            char[] original = frase.ToCharArray();
 
-            for (int i = 0; i < text.Length; i++)
+            for (int i = 0; i < original.Length; i++)
             {
-                int aux = searchInAlphabet(text[i]);
-                if (aux == -1)
+                char originalChar = original[i];
+
+                if (originalChar == ' ')
                 {
-                    encryptedText += text[i];
+                    nuevaFrase += originalChar;
                 }
                 else
                 {
-                    if (flag)
-                        encryptedText += char.ToUpper(newAlphabet[aux]);
+                    int shift = searchInAlphabeth(alfabeto, originalChar) + distancia;
+                    char newChar = ' ';
+
+                    while (shift >= alfabeto.Length)
+                    {
+                        shift = shift - alfabeto.Length;
+                    }
+
+                    if (Char.IsUpper(originalChar))
+                    {
+                        newChar = alfabeto.ElementAt(shift);
+                    }
                     else
-                        encryptedText += newAlphabet[aux];
+                    {
+                        newChar = Char.ToLower(alfabeto.ElementAt(shift));
+                    }
+                    nuevaFrase += newChar;
                 }
+
             }
 
             //escribir archivo 
@@ -117,45 +70,7 @@ namespace lab6
 
             using (StreamWriter file = new StreamWriter(fullPath))
             {
-                file.WriteLine(encryptedText);
-                file.Close();
-            }
-        }
-
-        public void decipher(StringBuilder input, string fileName)
-        {
-            string decipherText = "";
-            string text = input.ToString();
-            text = input.ToString();
-            //eliminar el caracter \r\n que agrega el stringbuilder al final de todo el texto
-            text = text.Remove(text.Length - 1);
-            text = text.Remove(text.Length - 1);
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                int aux = searchInNewAlphabet(text[i]);
-                if (aux == -1)
-                {
-                    decipherText += text[i];
-                }
-                else
-                {
-                    if (flag)
-                        decipherText += char.ToUpper(originalAlphabet[aux]);
-                    else
-                        decipherText += originalAlphabet[aux];
-                }
-            }
-
-            //escribir archivo 
-            string folder = @"C:\LABORATORIO6\";
-            string fullPath = folder + fileName;
-            // crear el directorio
-            DirectoryInfo directory = Directory.CreateDirectory(folder);
-
-            using (StreamWriter file = new StreamWriter(fullPath))
-            {
-                file.WriteLine(decipherText);
+                file.WriteLine(nuevaFrase);
                 file.Close();
             }
         }
