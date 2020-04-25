68ba98b869917cb3451a06db2dd5a897669f676e
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-04-25 17:50:32 -0600

    Endpoint para descifrar el texto usando cifrado cesar

--- Lab6/Lab6/Controllers/CipherController.cs
+++ Lab6/Lab6/Controllers/CipherController.cs
@@ -37,10 +37,30 @@ namespace lab6.Controllers
                     result.AppendLine(reader.ReadLine());
             }
 
+            //elimina caracteres extra que stringbuilder coloca al final de la cadena
             string text = result.ToString();
             text = text.Remove(text.Length - 1);
             text = text.Remove(text.Length - 1);
             Data.Instance.caesarCipher.cifrarCesar(text, Data.Instance.secretKey, file.FileName);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost, ActionName("caesar2Decipher")]
+        public IActionResult caesar2Decipher(IFormFile cipherFile)
+        {
+            //lectura del archivo
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(cipherFile.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadLine());
+            }
+
+            //elimina caracteres extra que stringbuilder coloca al final de la cadena
+            string text = result.ToString();
+            text = text.Remove(text.Length - 1);
+            text = text.Remove(text.Length - 1);
+            Data.Instance.caesarCipher.cifrarCesar(text, Data.Instance.secretKey, cipherFile.FileName);
             return RedirectToAction(nameof(Index));
         }
 
